299a025d6c9e56194e0abf8b562183b5fc2f1c37
Ferhat Uslu <ferhatuslu@stu.ayvansaray.edu.tr>
2024-09-30 15:05:49 +0300

    d
    d

--- dengnekirApp/lib/baslangicSayfa/baslangic1.dart
+++ dengnekirApp/lib/baslangicSayfa/baslangic1.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 Positioned.fill(
                   child:
-                    Image.asset('image/1-1.png',
+                    Image.asset('image/1.png',
                     fit: BoxFit.fill,//image tüm ekrana doldurma işlemi
                     ),
                 ),
